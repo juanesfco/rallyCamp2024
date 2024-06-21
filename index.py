fa831eb32c3549c4b93161ae94ff3238ffff2921
Juan Esteban Florez Coronel <juan.florez@upr.edu>
2024-06-21 19:52:18 -0400

    as

--- index.py
+++ index.py
@@ -4,7 +4,10 @@ from datetime import datetime
 import time
 import rallyFunctions as rf
 from streamlit_autorefresh import st_autorefresh
-count = st_autorefresh(interval=500, limit=1000, key="fizzbuzzcounter")
+
+st.write(st.secrets['admin'])
+
+#count = st_autorefresh(interval=500, limit=1000, key="fizzbuzzcounter")
  
 st.write("""
 Mathematical Rally - OMPR Camp 2024
@@ -14,15 +17,16 @@ st.write("""
 Hunger Games Version:
 """)
 
-tab = pd.read_pickle('players.df')
-st.table(tab)
-if st.checkbox('Kill: '):
-    number = st.number_input("Insert a number: ",value=None)
-    if type(number) == float:
-        rf.killPlayer(number)
+#tab = pd.read_pickle('players.df')
+#st.table(tab)
+#if st.checkbox('Kill: '):
+#    number = st.number_input("Insert a number: ",value=None)
+#    if type(number) == float:
+#        rf.killPlayer(number)
 
-if st.button('Reset game: '):
-    rf.resetTable()
+#if st.button('Reset game: '):
+#    rf.resetTable()
     
-if st.checkbox('Check count: '):
-    st.write(count)
+#if st.checkbox('Check count: '):
+#    st.write(count)
+
